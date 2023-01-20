87b45aea906311cdca6eaa5d95b249f47e3c8d2b
ganaranjo <ganaranjo@gmail.com>
2023-01-20 16:40:03 -0500

    Creacion parcial de clase Customer

--- spring-sakila-payment-service/src/main/java/com/arcadia/samples/sakila/payment/model/Customer.java
+++ spring-sakila-payment-service/src/main/java/com/arcadia/samples/sakila/payment/model/Customer.java
@@ -1,18 +1,113 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package com.arcadia.samples.sakila.payment.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Id;
+import java.sql.Timestamp;
+import java.util.Date;
 
 /**
  *
  * @author ganaranjo
-  * SMALLINT --> Short
- * TINYINT --> Byte
- * VARCHAR --> String
- * DATETIME --> Java.util.Date
- * TIMESTAMP --> Java.sql.Timestamp
 */
 public class Customer {
+
+    @Id
+    @Column(name = "customer_id")
+    private Short customerId;
     
+    @Column(name = "store_id")
+    private Byte storeId;
+    
+    @Column(name = "first_name")
+    private String firstName;
+    
+    @Column(name = "last_name")
+    private String lastName;
+    
+    @Column(name = "email")
+    private String email;
+    
+    @Column(name = "address_id")
+    private Short addressId;
+    
+    @Column(name = "actove")
+    private Byte active;
+    
+    @Column(name = "create_date")
+    private Date createDate;
+    
+    @Column(name = "last_update")
+    private Timestamp lastUpdate;
+
+    public Short getCustomerId() {
+        return customerId;
+    }
+
+    public void setCustomerId(Short customerId) {
+        this.customerId = customerId;
+    }
+
+    public Byte getStoreId() {
+        return storeId;
+    }
+
+    public void setStoreId(Byte storeId) {
+        this.storeId = storeId;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public Short getAddressId() {
+        return addressId;
+    }
+
+    public void setAddressId(Short addressId) {
+        this.addressId = addressId;
+    }
+
+    public Byte getActive() {
+        return active;
+    }
+
+    public void setActive(Byte active) {
+        this.active = active;
+    }
+
+    public Date getCreateDate() {
+        return createDate;
+    }
+
+    public void setCreateDate(Date createDate) {
+        this.createDate = createDate;
+    }
+
+    public Timestamp getLastUpdate() {
+        return lastUpdate;
+    }
+
+    public void setLastUpdate(Timestamp lastUpdate) {
+        this.lastUpdate = lastUpdate;
+    }
 }
