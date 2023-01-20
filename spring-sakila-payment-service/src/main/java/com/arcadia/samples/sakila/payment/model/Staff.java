1f7d8d2c5efb8c9a8298c8ee632dd093ed70939c
ganaranjo <ganaranjo@gmail.com>
2023-01-20 16:34:30 -0500

    Creacion parcial de clase Staff

--- spring-sakila-payment-service/src/main/java/com/arcadia/samples/sakila/payment/model/Staff.java
+++ spring-sakila-payment-service/src/main/java/com/arcadia/samples/sakila/payment/model/Staff.java
@@ -1,8 +1,8 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package com.arcadia.samples.sakila.payment.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Id;
+import java.sql.Timestamp;
 
 /**
  *
@@ -15,4 +15,125 @@ package com.arcadia.samples.sakila.payment.model;
  */
 public class Staff {
     
+    @Id
+    @Column(name = "staff_id")
+    private Byte staffId;
+    
+    @Column(name = "first_name")
+    private String firstName;
+    
+    @Column(name = "last_name")
+    private String lastName;
+    
+    @Column(name = "address_id")
+    private Short addressId;
+    
+    @Column(name = "picture")
+    private Byte[] picture;
+    
+    @Column(name = "email")
+    private String email;
+    
+    @Column(name = "store_id")
+    private Byte storeId;
+    
+    @Column(name = "active")
+    private Byte active;
+    
+    @Column(name = "username")
+    private String username;
+    
+    @Column(name = "password")
+    private String password;
+    
+    @Column(name = "last_update")
+    private Timestamp lastUpdate;
+
+    public Byte getStaffId() {
+        return staffId;
+    }
+
+    public void setStaffId(Byte staffId) {
+        this.staffId = staffId;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public Short getAddressId() {
+        return addressId;
+    }
+
+    public void setAddressId(Short addressId) {
+        this.addressId = addressId;
+    }
+
+    public Byte[] getPicture() {
+        return picture;
+    }
+
+    public void setPicture(Byte[] picture) {
+        this.picture = picture;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public Byte getStoreId() {
+        return storeId;
+    }
+
+    public void setStoreId(Byte storeId) {
+        this.storeId = storeId;
+    }
+
+    public Byte getActive() {
+        return active;
+    }
+
+    public void setActive(Byte active) {
+        this.active = active;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public Timestamp getLastUpdate() {
+        return lastUpdate;
+    }
+
+    public void setLastUpdate(Timestamp lastUpdate) {
+        this.lastUpdate = lastUpdate;
+    }
 }
